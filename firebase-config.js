3233e6d1317e5a5cbe9ed63324dec09b3a8bc777
Mihai Bors <moreliciouscod@gmail.com>
2026-01-07 17:16:20 +0100

    small fixes

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUHn9DI0OPOpm5aIM1vNKWPCfGrVZwA-Y",
+  authDomain: "project1356-1ff4b.firebaseapp.com",
+  databaseURL:
+    "https://project1356-1ff4b-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "project1356-1ff4b",
+  storageBucket: "project1356-1ff4b.firebasestorage.app",
+  messagingSenderId: "186698070234",
+  appId: "1:186698070234:web:ae1919386ed9ce78c44de3",
+  measurementId: "G-VKZPKTSJHC",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
